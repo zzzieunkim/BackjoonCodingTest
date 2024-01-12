fc87d0c2ba56c54874e5bd344a2a3c48b06609d2
zzzieunkim <129809247+zzzieunkim@users.noreply.github.com>
2024-01-12 17:14:43 +0900

    [Bronze V] Title: A/B, Time: 224 ms, Memory: 17996 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main{
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a = sc.nextDouble();
+		double b = sc.nextDouble();
+		double result = a / b;
+		
+		System.out.println(result);
+	}
+
+}
